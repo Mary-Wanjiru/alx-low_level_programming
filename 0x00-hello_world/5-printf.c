1f1b9bb2cccc3fb823cb2567616df2390da0c1ac
Mary Wanjiru <001wanjiru@gmail.com>
2023-02-16 07:39:32 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - a C program that prints with printf function
+ * return:(0)
+*/
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
