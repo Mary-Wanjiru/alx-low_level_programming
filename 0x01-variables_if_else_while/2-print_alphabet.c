3132d23161aa7af42e56e94e7f7e8d7c72a1f775
Mary Wanjiru <001wanjiru@gmail.com>
2023-02-17 13:26:02 -0800

    task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description - a prgrm that prints in lowercase
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar (c);
+		c++;
+	}
+	{
+	putchar('\n');
+	}
+return (0);
+}
+
