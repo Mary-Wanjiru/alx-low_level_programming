baea8de532285b1532ba0f0d73a4e775524b4119
Mary Wanjiru <oo1wanjiru@gmail.com>
2023-05-18 06:31:38 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,13 @@
 
 /**
  * main - Entry point
+ * Description: A program that uses the puts function
  *
  * Return: 0
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-		return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
