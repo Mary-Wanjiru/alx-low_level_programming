d603205f42093948589fec00d192705a0867363f
Mary Wanjiru <oo1wanjiru@gmail.com>
2023-05-18 10:49:52 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,8 +11,7 @@
 
 int main(void)
 {
-	write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar,
-	2015-10-19\n", 59);
+	write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar 2015-10-19\n", 59);
 	write(2, "\n", 1);
 	return (1);
 }
