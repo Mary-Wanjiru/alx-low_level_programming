8cb15f41eae1d23017c4994057226339bb57ab82
Mary Wanjiru <oo1wanjiru@gmail.com>
2023-05-20 02:48:51 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * descripion: This program that prints the alphabet in lowercase
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar(c + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
