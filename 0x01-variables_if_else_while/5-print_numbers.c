5e4773d253bcf1fa368a328418e7b2fc05584196
Mary Wanjiru <oo1wanjiru@gmail.com>
2023-05-20 02:32:50 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * descripion: This program that prints all single numbers of base 10 from 0
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		printf("%d", c);
+	}
+	printf("\n");
+	return (0);
+}
