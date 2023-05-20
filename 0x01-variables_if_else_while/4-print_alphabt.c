079ff56ab54c82c08b5606ad84a77d151e283afd
Mary Wanjiru <oo1wanjiru@gmail.com>
2023-05-20 02:15:38 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description - a program that prints alpabet in lowercase except q and e
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		
+		if ( c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+
+	putchar('\n');
+
+	return (0);
+}
