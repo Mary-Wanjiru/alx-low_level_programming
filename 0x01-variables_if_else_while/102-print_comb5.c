d5e35541442f998b6392957cb2ff3703b74e57f0
Mary Wanjiru <oo1wanjiru@gmail.com>
2023-05-22 06:38:09 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,18 +14,22 @@ int main(void)
 
 	for (a = 0; a <= 99; a++)
 	{
-		for (b = a + 1; b <= 99; b++)
+		for (b = 0; b <= 99; b++)
 		{
+			if (a < b && a != b)
+			{
 			putchar('0' + (a / 10));
 			putchar('0' + (a % 10));
 			putchar(' ');
 			putchar('0' + (b / 10));
 			putchar('0' + (b % 10));
-		}
+		
 		if (a != 98 || b != 99)
 		{
 			putchar(',');
 			putchar(' ');
+		}
+			}
 		}
 	}
 	putchar('\n');
